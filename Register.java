6918e0438ad78be9ef92f8a81b03323e0c8d04be
ABHIJEET GHORPADE <you@example.com>
2024-10-17 21:14:46 +0530

    methods in java

---
+++ Register.java
@@ -0,0 +1,23 @@
+public class Register{
+	public static void main(String[] args){
+		insert();
+		insert();
+		create();
+		insert();
+		create();
+
+
+	}
+
+	public static void insert(){
+		System.out.println("i have registerd to github account");
+		System.out.println("its very usefull");
+	}
+
+	public static void create(){
+		System.out.println("how are you");
+		System.out.println("im fine what about you?");
+	}
+
+
+}
