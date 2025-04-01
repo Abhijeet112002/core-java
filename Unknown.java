c9c915a1b6453ab421c0060fcb3485e1e126c678
Abhijeet <a37abhijeetghorpade@gmail.com>
2025-04-01 12:39:05 +0530

    variables

---
+++ Unknown.java
@@ -0,0 +1,77 @@
+public class Unknown{
+
+	public static void main(String[] args){
+
+	int number = 12;// declaration of variable and intialization
+    number = 15; //resizing
+
+int price = 700;
+price = 500;
+
+int mobno = 96;
+mobno = 74;
+
+int doby = 2002;
+doby = 2003;
+
+int sem = 7;
+sem = 8;
+
+int salary = 50;
+salary = 10;
+
+int bonous = 10;
+bonous = 50;
+
+int coursefee = 5;
+coursefee = 40;
+
+int rank = 1;
+rank = 2;
+
+int usn = 001;
+usn = 002;
+
+int subcode = 71;
+subcode = 72;
+
+int laptopprice = 60;
+laptopprice = 45;
+
+int distance = 35;
+distance = 48;
+
+int listnumber = 2;
+listnumber = 3;
+
+int roomno = 7;
+roomno = 5;
+
+int rent = 4000;
+rent = 3939;
+
+int atmpin = 101;
+atmpin = 32;
+
+System.out.println(number);
+System.out.println(price);
+System.out.println(mobno);
+System.out.println(doby);
+System.out.println(sem);
+System.out.println(salary);
+System.out.println(bonous);
+System.out.println(coursefee);
+System.out.println(rank);
+System.out.println(usn);
+System.out.println(subcode);
+System.out.println(distance);
+System.out.println(laptopprice);
+System.out.println(listnumber);
+System.out.println(roomno);
+System.out.println(rent);
+System.out.println(atmpin);
+
+	
+	}
+}
+
