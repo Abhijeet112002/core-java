6111d00c8c67325770b666b8c38ab77cb7c7bf7f
Abhijeet <a37abhijeetghorpade@gmail.com>
2025-04-01 12:44:47 +0530

    Add files via upload
    methods in java

--- Register.java
+++ Register.java
@@ -9,15 +9,24 @@ public class Register{
 
 	}
 
+
 	public static void insert(){
 		System.out.println("i have registerd to github account");
 		System.out.println("its very usefull");
 	}
 
+
+
 	public static void create(){
 		System.out.println("how are you");
 		System.out.println("im fine what about you?");
+
+
 	}
+
+	
+
+
 
 
 }
